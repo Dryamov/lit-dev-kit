58038d8d11f2dc458c08a3afc6be5b87f156d9f4
Dryamov Vyacheslav <dryamov.dev@gmail.com>
2020-09-12 16:41:36 +0000

    FIX yarn

--- .devcontainer/library-scripts/node-debian.sh
+++ .devcontainer/library-scripts/node-debian.sh
@@ -3,4 +3,6 @@ DEBIAN_FRONTEND=noninteractive
 curl -sL https://deb.nodesource.com/setup_lts.x | bash -
 apt-get install -y nodejs
 
-curl -o- -L https://yarnpkg.com/install.sh | bash
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+     sudo apt-get update && sudo apt-get install yarn
